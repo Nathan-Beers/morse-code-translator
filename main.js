8f99ddfe8f86b94a368d3cf21620c78cca690cd9
Nathan <nathanbeers@hotmail.co.uk>
2022-01-18 15:17:28 +0000

    Rough psuedocode to lay out functionality

---
+++ main.js
@@ -0,0 +1,9 @@
+//class to store characters of morse code translator
+
+//function to find if a character is included in the array and store it in a new array
+
+//function that converts the new array into the matching characters
+
+//function that splits the new array for form a proper string
+
+//function that displays the new array to the DOM
