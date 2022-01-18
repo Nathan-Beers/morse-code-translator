915d4efa367222b52f619b1c0b76fb3a72b4aed1
Nathan <nathanbeers@hotmail.co.uk>
2022-01-18 15:41:57 +0000

    built storage of characters and simple viable product

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
 //class to store characters of morse code translator
+// let input = document.getElementById("input").innerHTML;
+
+class Morse {
+    constructor(character) {
+        this.character = character;
+        this.A = ".-";
+        this.B = "-...";
+        this.C = "-.-.";
+        this.D = "-..";
+        this.E = ".";
+        this.F = "..-.";
+        this.G = "--.";
+        this.H = "....";
+        this.I = "..";
+        this.J = ".---";
+        this.K = "-.-";
+        this.L = ".-..";
+        this.M = "--";
+        this.N = "-.";
+        this.O = "---";
+        this.P = ".--.";
+        this.Q = "--.-";
+        this.R = ".-.";
+        this.S = "...";
+        this.T = "-";
+        this.U = "..-";
+        this.V = "...-";
+        this.W = ".--";
+        this.X = "-..-";
+        this.Y = "-.--";
+        this.Z = "--..";
+    }
+
+    findCharacter() {
+        switch(this.character){
+            case ".-":
+                return "A";
+                break;
+            case "-...":
+                return "B";
+        
+        }
+    }
+}
+
+const translator = new Morse("-...");
+
+console.log(translator.findCharacter())
 
 //function to find if a character is included in the array and store it in a new array
 
